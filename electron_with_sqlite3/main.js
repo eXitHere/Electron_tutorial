c092fd50eb1afcd4aca6691d3931f88a2d92927e
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2021-01-17 16:45:53 +0700

    first commit

---
+++ electron_with_sqlite3/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron');
+const ipc = require('electron').ipcMain
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('./index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if(BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
+
+ipc.on('test', function(event) {
+  console.log("Hello world");
+})
